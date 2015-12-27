de3046956dbbc0e5b514658bd944571b225241e7
Doug Alderman <agnadcp-d1@yahoo.com>
2015-12-27 13:02:05 -0700

    12/27/15 changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-var box = document.querySelector('.box');
-box.addEventListener('transitionend', onTransitionEnd, false);
+$(document).ready(function() {
 
-function onTransitionEnd() {
-  box.classList.add('rotate');
-}
+    $('.box').on('click', function() {
+      /* e.preventDefault();
+      var href = $(this).attr('href');
+      // Delay setting the location for one second
+     setTimeout(function() {window.location = href},
+                          1000); */
+       $(this).addClass('openDoor');
+    });
+
+});
